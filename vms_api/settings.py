6b1683def761ecd62b880591ee0c844ec535f760
Selorm-best <83176869+Selorm-best@users.noreply.github.com>
2023-10-11 22:42:33 +0000

    Added a requirements.txt and prepared the app to be deployed on vercel

--- vms_api/settings.py
+++ vms_api/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tx9ljw4idhxl-5-*d(d%j)*$7bhdg4=fzin0#$-1!h@occ^25m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
