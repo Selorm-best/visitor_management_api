ce30085e23cb85c5367d86120d13728de7ed900b
Selorm-best <83176869+Selorm-best@users.noreply.github.com>
2023-10-17 15:24:02 +0000

    adding the destination urls

--- visitors/urls.py
+++ visitors/urls.py
@@ -15,7 +15,14 @@ urlpatterns =[
     path('visits/create/', views.VisitView.createVisit, name='visit-create'),
     path('visits/<int:pk>/update/', views.VisitView.updateVisit, name='visit-update'),
     path('visits/<int:pk>/', views.VisitView.getVisit, name='visit-retrieve'),
-    path('visits/<int:pk>/delete/', views.VisitView.deleteVisit, name='visit-delete')
+    path('visits/<int:pk>/delete/', views.VisitView.deleteVisit, name='visit-delete'),
+
+    
+    path('destination/', views.DestinationView.getDestinations, name='destinations-retrieve'),
+    path('destination/create/', views.DestinationView.createDestination, name='destination-create'),
+    path('destination/<int:pk>/update/', views.DestinationView.updateDestination, name='destination-update'),
+    path('destination/<int:pk>/', views.DestinationView.getDestination, name='destination-retrieve'),
+    path('destination/<int:pk>/delete/', views.DestinationView.deleteDestination, name='destination-delete'),
 
 
 
